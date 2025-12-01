fd651ffdfb5dc8c04b2797f5f6c6bc656faef2d5
Łukasz Duda <lukasz.duda.pl@gmail.com>
2025-12-01 20:35:21 +0100

    Change BPM

--- src/simple-metronome.tsx
+++ src/simple-metronome.tsx
@@ -1,12 +1,26 @@
+import { useState, type SyntheticEvent } from "react";
 import { play } from "./audio";
 import { useMetronome } from "./metronome";
 
 export function SimpleMetronome() {
+  const [bpm, setBpm] = useState(100);
+
+  function changeBpm(event: SyntheticEvent<HTMLInputElement>) {
+    const value = event.currentTarget.value;
+    const newBpm = Number(value);
+    setBpm(newBpm);
+  }
+
   const { start, stop } = useMetronome({ onBeat: play });
 
   return (
     <>
-      <button onClick={() => start(100)}>play</button>
+      <input
+        type="number"
+        value={bpm}
+        onChange={changeBpm}
+      />
+      <button onClick={() => start(bpm)}>play</button>
       <button onClick={stop}>stop</button>
     </>
   );
